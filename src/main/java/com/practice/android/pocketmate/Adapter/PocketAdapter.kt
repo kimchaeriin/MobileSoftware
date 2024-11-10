55423475cac8cd984b62f17476eb02fed1663fbf
Noname0430 <cl01004@naver.com>
2024-11-10 21:15:49 +0900

    PocketData -> PocketModel

--- src/main/java/com/practice/android/pocketmate/Adapter/PocketAdapter.kt
+++ src/main/java/com/practice/android/pocketmate/Adapter/PocketAdapter.kt
@@ -1,8 +1,9 @@
-import PocketData
+
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.practice.android.pocketmate.Model.PocketModel
 import com.practice.android.pocketmate.databinding.ItemPocketBinding
 
 
@@ -10,20 +11,20 @@ class PocketViewHolder(val binding: ItemPocketBinding):
     RecyclerView.ViewHolder(binding.root){
         private val context = binding.root.context
 
-        fun bind(item : PocketData){
+        fun bind(item: PocketModel){
             itemView.setOnClickListener{
-                val intent : Intent = Intent(context,PocketActivity::class.java)
+                val intent: Intent = Intent(context, PocketActivity::class.java)
                 intent.run{context.startActivity(this)}
             }
         }
     }
 
-class PocketAdapter(val Items:MutableList<PocketData>):
+class PocketAdapter(val Items:MutableList<PocketModel>):
     RecyclerView.Adapter<PocketViewHolder>(){
     override fun getItemCount(): Int = Items.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PocketViewHolder =
-        PocketViewHolder(ItemPocketBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        PocketViewHolder(ItemPocketBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
     override fun onBindViewHolder(holder: PocketViewHolder, position: Int) {
         val binding = (holder as PocketViewHolder).binding
--- src/main/java/com/practice/android/pocketmate/Pocket/MyPocketBoardActivity.kt
+++ src/main/java/com/practice/android/pocketmate/Pocket/MyPocketBoardActivity.kt
@@ -1,10 +1,10 @@
-import PocketData
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.practice.android.pocketmate.Model.PocketModel
 import com.practice.android.pocketmate.Pocket.WritePocketBoardActivity
 import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ActivityMyPocketBoardBinding
@@ -17,9 +17,9 @@ class MyPocketBoardActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val items = mutableListOf<PocketData>() //input data
+        val items = mutableListOf<PocketModel>() //input data
         for(i in 1..5){
-            items.add(PocketData("제목","내용", R.drawable.ic_launcher_background))
+            items.add(PocketModel("제목", "내용", R.drawable.ic_launcher_background))
         }
         binding.recyclerView.layoutManager= LinearLayoutManager(this)
         binding.recyclerView.adapter = PocketAdapter(items)
