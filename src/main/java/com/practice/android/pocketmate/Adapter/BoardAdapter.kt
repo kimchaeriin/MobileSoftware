b058c2e5cf9a36861c6f455b0a8d2771836461ad
Noname0430 <cl01004@naver.com>
2024-11-11 12:39:13 +0900

    BoardAdapter user->writer 변경

--- src/main/java/com/practice/android/pocketmate/Adapter/BoardAdapter.kt
+++ src/main/java/com/practice/android/pocketmate/Adapter/BoardAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.practice.android.pocketmate.Model.BoardModel
@@ -12,7 +13,7 @@ import com.practice.android.pocketmate.databinding.ItemBoardBinding
 
 class BoardViewHolder(val binding: ItemBoardBinding) : RecyclerView.ViewHolder(binding.root)
 
-class BoardAdapter (val context: Context, val items: MutableList<BoardModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class BoardAdapter (val items: MutableList<BoardModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         BoardViewHolder(ItemBoardBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
@@ -21,10 +22,13 @@ class BoardAdapter (val context: Context, val items: MutableList<BoardModel>): R
         binding.boardTitle.text = items[position].title
         binding.boardContent.text = items[position].content
         binding.boardImage.setImageResource(items[position].image)
-        binding.root.setOnClickListener { //수정 필요
-            val intent = Intent(context, TipActivity::class.java)
-            intent.putExtra("key", holder.itemId)
-            context.startActivity(intent)
+//        binding.root.setOnClickListener { //수정 필요
+//            val intent = Intent(context, TipActivity::class.java)
+//            intent.putExtra("key", holder.itemId)
+//            context.startActivity(intent)
+//        }
+        binding.root.setOnClickListener {
+
         }
     }
 
