7fca79bc77e5de2f7b4baddfcaed6b1b237d5376
Noname0430 <cl01004@naver.com>
2024-11-14 11:20:45 +0900

    오타 수정

--- src/main/java/com/practice/android/pocketmate/Auth/SettingActivity.kt
+++ src/main/java/com/practice/android/pocketmate/Auth/SettingActivity.kt
@@ -1,12 +1,16 @@
 package com.practice.android.pocketmate.Auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.practice.android.pocketmate.MainActivity
 import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ActivitySettingBinding
+import com.practice.android.pocketmate.util.FBAuth
+import com.practice.android.pocketmate.util.FBRef
 
 class SettingActivity : AppCompatActivity() {
     lateinit var binding : ActivitySettingBinding
@@ -16,6 +20,15 @@ class SettingActivity : AppCompatActivity() {
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.writeNicknameBtn.setOnClickListener {
+            FBRef.nicknameRef.child(FBAuth.getUid()).setValue(binding.writeNicknameArea.text.toString())
+            switchScreen(this, MainActivity::class.java)
+            finish()
+        }
+    }
 
+    private fun switchScreen(from: AppCompatActivity, to: Class<out AppCompatActivity>) {
+        val intent = Intent(from, to)
+        from.startActivity(intent)
     }
 }
