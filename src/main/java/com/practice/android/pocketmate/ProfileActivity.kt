50c5fb408e269a2a3dc673037566c7f6f2d84a8b
Noname0430 <cl01004@naver.com>
2024-11-15 12:29:08 +0900

    닉네임 변경 버튼 클릭 후 키보드 닫기 기능 추가, refactoring

--- src/main/java/com/practice/android/pocketmate/ProfileActivity.kt
+++ src/main/java/com/practice/android/pocketmate/ProfileActivity.kt
@@ -5,8 +5,11 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Contacts.Settings.setSetting
 import android.text.Editable
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.getSystemService
@@ -27,7 +30,7 @@ import com.practice.android.pocketmate.util.FBRef
 class ProfileActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityProfileBinding
-
+    var imm : InputMethodManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,20 +39,28 @@ class ProfileActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+
+        getProfile()
+        handleBtns()
+    }
+
+    private fun getProfile() {
         val uid = FBAuth.getUid()
-
         binding.uid.text = uid
-
         getNickname { nickname ->
             binding.nickname.setText(nickname)
         }
+    }
 
+    private fun handleBtns() {
         binding.profileImageBtn.setOnClickListener {
             //프로필 이미지 바꾸기
         }
 
         binding.changeBtn.setOnClickListener {
             changeNickname()
+            hideKeyBoard()
         }
 
         binding.copyBtn.setOnClickListener {
@@ -163,5 +174,12 @@ class ProfileActivity : AppCompatActivity() {
         val intent = Intent(from, to)
         startActivity(intent)
         finish()
+    }
+
+    fun hideKeyBoard() {
+        val view = currentFocus
+        if (view != null) {
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
