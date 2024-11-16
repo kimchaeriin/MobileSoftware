ed9c08c5229c11708ba9e66328c995a744b2df51
Noname0430 <cl01004@naver.com>
2024-11-17 00:41:54 +0900

    텍스트 색 변경 기능 추가, 수정 기능 보완 + 이후 추가 보완 필요

--- src/main/java/com/practice/android/pocketmate/Tip/EditTipActivity.kt
+++ src/main/java/com/practice/android/pocketmate/Tip/EditTipActivity.kt
@@ -1,8 +1,13 @@
 package com.practice.android.pocketmate.Tip
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.GridLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,29 +16,33 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.practice.android.pocketmate.Model.BoardModel
 import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ActivityEditTipBinding
+import com.practice.android.pocketmate.databinding.ActivityTipBinding
 import com.practice.android.pocketmate.util.FBAuth
 import com.practice.android.pocketmate.util.FBRef
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.threeten.bp.LocalDate
 
 class EditTipActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityEditTipBinding
+    lateinit var tipBinding: ActivityTipBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditTipBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        tipBinding = ActivityTipBinding.inflate(layoutInflater)
+        AndroidThreeTen.init(this)
 
         val key = intent.getStringExtra("key").toString()
 
         getTipData(key)
-
-        binding.editBtn.text = "수정하기"
-        binding.editBtn.setOnClickListener {
-            editAndSwitchScreen(key)
-        }
+        handleBtns(key)
     }
 
     private fun getTipData(key : String) {
@@ -42,7 +51,10 @@ class EditTipActivity : AppCompatActivity() {
                 val tip = dataSnapshot.getValue(BoardModel::class.java)!!
                 binding.title.setText(tip.title)
                 binding.content.setText(tip.content)
-                binding.image.setImageResource(tip.image)
+                if (tip.image != 0) {
+                    binding.image.visibility = View.VISIBLE
+                    binding.image.setImageResource(tip.image)
+                }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -52,39 +64,94 @@ class EditTipActivity : AppCompatActivity() {
         FBRef.tipRef.child(key).addValueEventListener(postListener)
     }
 
+    private fun handleBtns(key: String) {
+        binding.editBtn.setOnClickListener {
+            editAndSwitchScreen(key)
+        }
+
+        binding.textColorChange.setOnClickListener {
+            changeTextColor()
+        }
+
+        binding.imageBtn.setOnClickListener {
+            //이미지 추가
+        }
+    }
+
+    private fun changeTextColor() {
+        showColorPickerDialog { selectedColor ->
+            binding.content.setTextColor(selectedColor)
+            binding.textColorChange.setTextColor(selectedColor)
+        }
+    }
+
+    private fun showColorPickerDialog(onColorSelected: (Int) -> Unit) {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_color_picker)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val colorPalette = dialog.findViewById<GridLayout>(R.id.color_palette)
+
+        val colorArray = listOf(
+            Color.RED, Color.GREEN, Color.BLUE,
+            Color.YELLOW, Color.CYAN, Color.MAGENTA,
+            Color.BLACK, Color.GRAY
+        )
+
+        colorArray.forEach { color ->
+            val colorButton = Button(this).apply {
+                layoutParams = GridLayout.LayoutParams().apply {
+                    width = 100
+                    height = 100
+                    setMargins(10, 10, 10, 10)
+                }
+                setBackgroundColor(color)
+            }
+
+            colorButton.setOnClickListener {
+                onColorSelected(color)
+                dialog.dismiss()
+            }
+
+            colorPalette.addView(colorButton)
+        }
+
+        dialog.show()
+    }
+
     private fun editAndSwitchScreen(key: String) {
+        val uid = FBAuth.getUid()
         val title = binding.title.text.toString()
+        val date = LocalDate.now().toString()
         val content = binding.content.text.toString()
-        val image = 0 //null일 때와 아닐 때 분리 필요
-        val user = FBAuth.getUid()
-        val agree = 0
-        val disagree = 0
+        val image = 0
+        val agree = getAgree()
+        val disagree = getDisagree()
 
         if (title.isEmpty() || content.isEmpty()) {
             Toast.makeText(this, "제목과 내용은 한 글자 이상 작성해야 합니다.", Toast.LENGTH_SHORT).show()
         }
         else {
-            getNickname { nickname ->
-                val tip = BoardModel(user, nickname, title, content, image, agree, disagree)
-                FBRef.tipRef.child(key).setValue(tip)
-            }
+            val tip = BoardModel(uid, date, title, content, image, agree, disagree)
+            val tipValues = tip.toMap()
+
+            val childUpdates = hashMapOf<String, Any>(
+                "/TipBoard/$key" to tipValues,
+            )
+            val databaseRef = Firebase.database.reference
+            databaseRef.updateChildren(childUpdates)
+
             switchScreen(this, TipBoardActivity::class.java)
         }
     }
 
-    private fun getNickname(callback: (String) -> Unit) {
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val nickname = dataSnapshot.getValue(String::class.java) ?: ""
-                callback(nickname)
-            }
+    private fun getAgree(): Int {
+        return tipBinding.agreeNumber.toString().toInt()
+    }
 
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                callback("") // 에러가 발생한 경우 빈 문자열을 콜백으로 전달
-            }
-        }
-        FBRef.nicknameRef.child(FBAuth.getUid()).addListenerForSingleValueEvent(postListener)
+    private fun getDisagree() : Int {
+        return tipBinding.disagreeNumber.toString().toInt()
     }
 
     private fun switchScreen(from: AppCompatActivity, to: Class<out AppCompatActivity>) {
--- src/main/java/com/practice/android/pocketmate/Tip/WriteTipActivity.kt
+++ src/main/java/com/practice/android/pocketmate/Tip/WriteTipActivity.kt
@@ -1,10 +1,16 @@
 package com.practice.android.pocketmate.Tip
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.GridLayout
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -12,9 +18,13 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.practice.android.pocketmate.Model.BoardModel
+import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ActivityWriteTipBinding
 import com.practice.android.pocketmate.util.FBAuth
 import com.practice.android.pocketmate.util.FBRef
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.threeten.bp.LocalDate
+
 
 class WriteTipActivity : AppCompatActivity() {
 
@@ -24,16 +34,68 @@ class WriteTipActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteTipBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        AndroidThreeTen.init(this)
+        handleBtns()
+    }
 
+    private fun handleBtns() {
         binding.postBtn.setOnClickListener {
             postAndSwitchScreen()
         }
+
+        binding.exitBtn.setOnClickListener {
+            switchScreen(this, TipBoardActivity::class.java)
+            finish()
+        }
+
+        binding.textColorChange.setOnClickListener {
+            changeTextColor()
+        }
+    }
+
+    private fun changeTextColor() {
+        showColorPickerDialog { selectedColor ->
+            binding.content.setTextColor(selectedColor)
+            binding.textColorChange.setTextColor(selectedColor)
+        }
+    }
+
+    private fun showColorPickerDialog(onColorSelected: (Int) -> Unit) {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.dialog_color_picker)
+
+            val colorPalette = dialog.findViewById<GridLayout>(R.id.color_palette)
+
+            val colorArray = listOf(
+                Color.RED, Color.GREEN, Color.BLUE,
+                Color.YELLOW, Color.CYAN, Color.MAGENTA,
+                Color.BLACK, Color.GRAY
+            )
+
+            colorArray.forEach { color ->
+                val colorButton = Button(this).apply {
+                    layoutParams = GridLayout.LayoutParams().apply {
+                        width = 100
+                        height = 100
+                        setMargins(10, 10, 10, 10)
+                    }
+                    setBackgroundColor(color)
+                }
+
+                colorButton.setOnClickListener {
+                    onColorSelected(color)
+                    dialog.dismiss()
+                }
+
+                colorPalette.addView(colorButton)
+            }
+
+            dialog.show()
     }
 
     private fun postAndSwitchScreen() {
         val user = FBAuth.getUid()
+        val date = LocalDate.now().toString()
         val title = binding.title.text.toString()
         val content = binding.content.text.toString()
         val image = 0 //null일 때와 아닐 때 분리 필요
@@ -44,28 +106,12 @@ class WriteTipActivity : AppCompatActivity() {
             Toast.makeText(this, "제목과 내용은 한 글자 이상 작성해야 합니다.", Toast.LENGTH_SHORT).show()
         }
         else {
-            getNickname(user) { nickname ->
-                val key = FBRef.tipRef.push().key.toString()
-                val tip = BoardModel(user, nickname, title, content, image, agree, disagree)
-                FBRef.tipRef.child(key).setValue(tip)
-            }
+            val key = FBRef.tipRef.push().key.toString()
+            val tip = BoardModel(user, date, title, content, image, agree, disagree)
+            FBRef.tipRef.child(key).setValue(tip)
+
             switchScreen(this, TipBoardActivity::class.java)
         }
-    }
-
-    private fun getNickname(user: String, callback: (String) -> Unit) {
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val nickname = dataSnapshot.getValue(String::class.java) ?: ""
-                callback(nickname)
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                callback("") // 에러가 발생한 경우 빈 문자열을 콜백으로 전달
-            }
-        }
-        FBRef.nicknameRef.child(user).addListenerForSingleValueEvent(postListener)
     }
 
     private fun switchScreen(from: AppCompatActivity, to: Class<out AppCompatActivity>) {
