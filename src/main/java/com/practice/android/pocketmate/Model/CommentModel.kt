676643f772997c1d8258b95e79bcac3fd3a628cd
Noname0430 <cl01004@naver.com>
2024-11-19 21:50:25 +0900

    Delete all files from main branch

--- src/main/java/com/practice/android/pocketmate/Model/CommentModel.kt
+++
@@ -1,7 +0,0 @@
-package com.practice.android.pocketmate.Model
-
-data class CommentModel (
-    var image: Int = 0,
-    var uid: String = "",
-    var comment: String = ""
-)
--- src/main/java/com/practice/android/pocketmate/Model/PocketModel.kt
+++
@@ -1,7 +0,0 @@
-package com.practice.android.pocketmate.Model
-
-data class PocketModel (
-    var title : String = "",
-    var content : String = "",
-    var image : Int = 0
-)
