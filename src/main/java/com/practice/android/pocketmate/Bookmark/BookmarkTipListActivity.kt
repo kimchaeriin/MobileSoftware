80800e27eda24572c18f978c85bb7665ec143b01
Noname0430 <cl01004@naver.com>
2024-11-21 00:55:51 +0900

    Board 프래그먼트에서 북마크 기능 추가, BoardAdapter -> TipBoardAdapter 변경, refactoring

--- src/main/java/com/practice/android/pocketmate/Bookmark/BookmarkTipListActivity.kt
+++ src/main/java/com/practice/android/pocketmate/Bookmark/BookmarkTipListActivity.kt
@@ -1,46 +1,60 @@
 package com.practice.android.pocketmate.Bookmark
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.practice.android.pocketmate.Adapter.BookmarkAdapter
+import com.practice.android.pocketmate.Adapter.TipBoardAdapter
 import com.practice.android.pocketmate.Model.BoardModel
 import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ActivityBookmarkTipListBinding
 import com.practice.android.pocketmate.util.FBAuth
 import com.practice.android.pocketmate.util.FBRef
-import com.practice.android.pocketmate.util.ScreenUtils
+import com.practice.android.pocketmate.util.FBRef.Companion.bookmarkRef
+import com.practice.android.pocketmate.util.FBRef.Companion.tipRef
+import com.practice.android.pocketmate.util.ScreenUtils.Companion.setBottomNavigationBar
 
 class BookmarkTipListActivity : AppCompatActivity() {
     lateinit var binding : ActivityBookmarkTipListBinding
     private val tipList = mutableListOf<BoardModel>()
     private val keyList = mutableListOf<String>()
     private var bookmarkIdList = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookmarkTipListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.navigation.selectedItemId = R.id.nav_bookmark
-        ScreenUtils.setBottomNavigationBar(this, binding.navigation)
+        setNavigationBar()
+        getBookmarkedList()
+        if (bookmarkIdList.isNotEmpty()) {
+            binding.noTipText.visibility = View.GONE
+        }
+
+        setupRecyclerView()
     }
 
-    override fun onResume() {
-        super.onResume()
-        getBookmarkedList()
-        binding.recycler.adapter = BookmarkAdapter(this, tipList, keyList, bookmarkIdList)
+    private fun setNavigationBar() {
+        binding.navigation.selectedItemId = R.id.nav_bookmark
+        setBottomNavigationBar(this, binding.navigation)
+    }
+
+    private fun setupRecyclerView() {
+        binding.recycler.adapter = TipBoardAdapter(this, tipList, keyList, bookmarkIdList)
         binding.recycler.layoutManager = LinearLayoutManager(this)
     }
 
     private fun getBookmarkedList() {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                bookmarkIdList.clear()
                 for (data in dataSnapshot.children) {
-                    val key = data.getValue().toString()
-                    bookmarkIdList.add(key)
+                    binding.noTipText.visibility = View.GONE //있었다가 취소하면 문구가 안 보임
+                    bookmarkIdList.add(data.key.toString())
                 }
+                binding.recycler.adapter?.notifyDataSetChanged()
                 getTipList()
             }
 
@@ -48,12 +62,14 @@ class BookmarkTipListActivity : AppCompatActivity() {
                 ///
             }
         }
-        FBRef.bookmarkRef.child(FBAuth.getUid()).addValueEventListener(postListener)
+        bookmarkRef.child(FBAuth.getUid()).addValueEventListener(postListener)
     }
 
     private fun getTipList() {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                tipList.clear()
+                keyList.clear()
                 for (data in dataSnapshot.children) {
                     val key = data.key.toString()
                     if (bookmarkIdList.contains(key)) {
@@ -69,6 +85,6 @@ class BookmarkTipListActivity : AppCompatActivity() {
 
             }
         }
-        FBRef.tipRef.addValueEventListener(postListener)
+        tipRef.addValueEventListener(postListener)
     }
 }
