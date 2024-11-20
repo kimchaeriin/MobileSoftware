82eb0097fe87fb8c2b3ec88dd5159476f39c1cf4
Noname0430 <cl01004@naver.com>
2024-11-21 00:55:20 +0900

    Board 프래그먼트에서 북마크 기능 추가, BoardAdapter -> TipBoardAdapter 변경

--- src/main/java/com/practice/android/pocketmate/Tip/AllTipBoardFragment.kt
+++ src/main/java/com/practice/android/pocketmate/Tip/AllTipBoardFragment.kt
@@ -12,6 +12,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.practice.android.pocketmate.Adapter.BoardAdapter
+import com.practice.android.pocketmate.Adapter.TipBoardAdapter
 import com.practice.android.pocketmate.Model.BoardModel
 import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ActivityTipBoardBinding
@@ -34,6 +35,7 @@ class AllTipBoardFragment : Fragment() {
     private val binding get() = _binding!!
     private val tipList = mutableListOf<BoardModel>()
     private val keyList = mutableListOf<String>()
+    private val bookmarkedIdList = mutableListOf<String>()
     lateinit var boardAdapter: BoardAdapter
 
     override fun onCreateView(
@@ -44,16 +46,33 @@ class AllTipBoardFragment : Fragment() {
         _binding = FragmentAllTipBoardBinding.inflate(inflater, container, false)
 
         getTipList()
-
-        if (tipList.isNotEmpty()) {
-            binding.noTipText.visibility = View.GONE
-        }
-        boardAdapter = BoardAdapter(requireContext(), tipList, keyList)
-
-        binding.recycler.adapter = boardAdapter
-        binding.recycler.layoutManager = LinearLayoutManager(context)
+        getBookmarkedIdList()
+        setupRecyclerView()
 
         return binding.root
+    }
+
+    private fun getBookmarkedIdList() {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                bookmarkedIdList.clear()
+                for (data in dataSnapshot.children) {
+                    bookmarkedIdList.add(data.key.toString())
+                }
+                binding.recycler.adapter?.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+            }
+        }
+        FBRef.bookmarkRef.child(FBAuth.getUid()).addValueEventListener(postListener)
+    }
+
+    private fun setupRecyclerView() {
+        boardAdapter = TipBoardAdapter(requireContext(), tipList, keyList, bookmarkedIdList)
+        binding.recycler.adapter = boardAdapter
+        binding.recycler.layoutManager = LinearLayoutManager(context)
     }
 
     private fun getTipList() {
@@ -71,6 +90,9 @@ class AllTipBoardFragment : Fragment() {
                 binding.recycler.adapter?.notifyDataSetChanged()
                 tipList.reverse()
                 keyList.reverse()
+                if (tipList.isNotEmpty()) {
+                    binding.noTipText.visibility = View.GONE
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
