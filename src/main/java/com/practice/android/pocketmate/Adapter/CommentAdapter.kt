b2a6e5a9457a0cdc455cab78f0346eb23969a9f7
Noname0430 <cl01004@naver.com>
2024-11-21 15:00:07 +0900

    댓글 삭제 기능 추가

--- src/main/java/com/practice/android/pocketmate/Adapter/CommentAdapter.kt
+++ src/main/java/com/practice/android/pocketmate/Adapter/CommentAdapter.kt
@@ -1,15 +1,17 @@
 package com.practice.android.pocketmate.Adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.practice.android.pocketmate.Model.CommentModel
 import com.practice.android.pocketmate.databinding.ItemCommentBinding
 import com.practice.android.pocketmate.util.FBAuth
+import com.practice.android.pocketmate.util.FBRef
 
 class CommentViewHolder(val binding: ItemCommentBinding) : RecyclerView.ViewHolder(binding.root)
 
-class CommentAdapter(val comments: MutableList<CommentModel>) : RecyclerView.Adapter<CommentViewHolder>() {
+class CommentAdapter(val boardKey: String, val comments: MutableList<CommentModel>, val commentKeyList: MutableList<String>) : RecyclerView.Adapter<CommentViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
         return CommentViewHolder(ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
@@ -19,6 +21,13 @@ class CommentAdapter(val comments: MutableList<CommentModel>) : RecyclerView.Ada
         val uid = comments[position].uid
         FBAuth.getNickname(uid) { nickname ->
             holder.binding.nickname.text = nickname
+        }
+        if (FBAuth.getUid() == comments[position].uid) {
+            holder.binding.deleteBtn.visibility = View.VISIBLE
+        }
+        holder.binding.deleteBtn.setOnClickListener {
+            comments.remove(comments[position])
+            FBRef.commentRef.child(boardKey).child(commentKeyList[position]).removeValue()
         }
     }
 
