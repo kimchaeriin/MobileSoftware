6e551851738120b1d4fb1b6a1dca63ae3bda8f95
Noname0430 <cl01004@naver.com>
2024-11-22 22:55:10 +0900

    FCM 클래스 생성

---
+++ src/main/java/com/practice/android/pocketmate/MyFirebaseMessagingService.kt
@@ -0,0 +1,52 @@
+package com.practice.android.pocketmate
+
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.IBinder
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val TAG = "FirebaseMessagingService"
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+//        sendRegistrationToServer(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        Log.d(TAG, "From: ${remoteMessage.from}")
+        val notification : Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//        val pendingIntent = PendingIntent.getActivity(this, )
+
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+//                // For long-running tasks (10 seconds or more) use WorkManager.
+//                scheduleJob()
+//            } else {
+//                // Handle message within 10 seconds
+//                handleNow()
+            }
+        }
+
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            val title = it.title
+            val body = it.body
+        }
+    }
+}
