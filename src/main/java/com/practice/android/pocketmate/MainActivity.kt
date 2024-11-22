48f4525ce3d7d8bfaab2575b68fc147ef1757899
Noname0430 <cl01004@naver.com>
2024-11-22 18:58:20 +0900

    가로 스크롤 설정

--- src/main/java/com/practice/android/pocketmate/MainActivity.kt
+++ src/main/java/com/practice/android/pocketmate/MainActivity.kt
@@ -5,20 +5,23 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.practice.android.pocketmate.Tip.TipBoardActivity
 import com.practice.android.pocketmate.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.practice.android.pocketmate.Adapter.ViewPagerAdapter
 import com.practice.android.pocketmate.Model.BoardModel
 import com.practice.android.pocketmate.Pocket.PocketBoardActivity
 import com.practice.android.pocketmate.friends.FriendsListActivity
 import com.practice.android.pocketmate.util.ScreenUtils
 import com.practice.android.pocketmate.util.FBRef
 
-
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val pocket = false
+    private val tip = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,29 +34,34 @@ class MainActivity : AppCompatActivity() {
 
         binding.navigationDrawer.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.nav_drawer_friends -> { ScreenUtils.switchScreen(this,FriendsListActivity::class.java)}
-                R.id.nav_drawer_settings -> { ScreenUtils.switchScreen(this,SettingsActivity::class.java) }
+                R.id.nav_drawer_friends -> { ScreenUtils.switchScreen(this, FriendsListActivity::class.java)}
+                R.id.nav_drawer_settings -> { ScreenUtils.switchScreen(this, SettingsActivity::class.java) }
             }
             binding.drawerMain.closeDrawers()
             true
         }
-
         getRecentTip()
         getRecentPocket()
-        setClickTextView()
+        binding.pocketViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.tipViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         ScreenUtils.setBottomNavigationBar(this, binding.navigation)
     }
 
     private fun getRecentTip() {
+        val tipList = mutableListOf<BoardModel>()
+        val tipKeyList = mutableListOf<String>()
         FBRef.tipRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for (data in dataSnapshot.children.reversed()) {
+                tipList.clear()
+                tipKeyList.clear()
+                val dataList = dataSnapshot.children.reversed()
+                for (data in dataList) {
                     val tip = data.getValue(BoardModel::class.java)
-                    binding.tipTitle.text = tip?.title
-                    binding.tipContent.text = tip?.content
-                    break
+                    tipList.add(tip!!)
+                    tipKeyList.add(data.key.toString())
                 }
+                binding.tipViewPager.adapter = ViewPagerAdapter(this@MainActivity, tipList, tipKeyList, tip)
             }
             override fun onCancelled(error: DatabaseError) {
                 //읽기 실패
@@ -62,32 +70,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getRecentPocket() {
+        val pocketList = mutableListOf<BoardModel>()
+        val pocketKeyList = mutableListOf<String>()
         FBRef.pocketRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for (data in dataSnapshot.children.reversed()) {
+                pocketList.clear()
+                pocketKeyList.clear()
+                val dataList = dataSnapshot.children.reversed()
+                for (data in dataList) {
                     val pocket = data.getValue(BoardModel::class.java)
-                    binding.pocketTitle.text = pocket?.title
-                    binding.pocketContent.text = pocket?.content
-                    break
+                    pocketList.add(pocket!!)
+                    pocketKeyList.add(data.key.toString())
                 }
+                binding.pocketViewPager.adapter = ViewPagerAdapter(this@MainActivity, pocketList, pocketKeyList, pocket)
             }
+
             override fun onCancelled(error: DatabaseError) {
                 //읽기 실패
             }
         })
     }
-
-    private fun setClickTextView() {
-        binding.pocket.setOnClickListener(){
-            val intent: Intent = Intent(this, PocketBoardActivity::class.java)
-            startActivity(intent)
-
-        }
-        binding.tip.setOnClickListener(){
-            val intent: Intent = Intent(this, TipBoardActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-
 }
