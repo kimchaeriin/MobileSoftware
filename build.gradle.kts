a7d42e95aecde4d2f2b2899472d1daff3103da9c
Noname0430 <cl01004@naver.com>
2024-11-22 22:54:15 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ android {
 dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation ("com.jakewharton.threetenabp:threetenabp:1.3.0")
-    implementation("com.google.firebase:firebase-database")
+    implementation ("com.google.firebase:firebase-database")
     implementation ("com.kakao.sdk:v2-user:2.20.6") // 카카오 로그인 API 모듈
     implementation ("com.kakao.sdk:v2-talk:2.20.6") // 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
     implementation ("com.kakao.sdk:v2-cert:2.20.6") // 카카오톡 인증 서비스 API 모듈
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.identity.credential.android)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
