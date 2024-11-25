d29be56f8b1a2c12d21b2cc3e3ab4c1c01ec441c
Han HaeWoon <dorothy8380@gmail.com>
2024-11-26 00:13:04 +0900

    파이어베이스 코드 추가

--- src/main/java/com/practice/android/pocketmate/friends/FriendsListActivity.kt
+++ src/main/java/com/practice/android/pocketmate/friends/FriendsListActivity.kt
@@ -1,11 +1,14 @@
 package com.practice.android.pocketmate.friends
 
-import FriendsListFragment
 import android.os.Bundle
-import android.view.Menu
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.practice.android.pocketmate.R
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.practice.android.pocketmate.databinding.ActivityFriendsListBinding
+import com.practice.android.pocketmate.util.FBAuth
+import com.practice.android.pocketmate.util.FBRef
 
 
 class FriendsListActivity : AppCompatActivity() {
@@ -15,31 +18,61 @@ class FriendsListActivity : AppCompatActivity() {
         binding = ActivityFriendsListBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbarFriendsList)
+        setSupportActionBar(binding.toolbarFriends)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val fragment = FriendsListFragment()
-        val fragmentManager = supportFragmentManager
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentFriends.id, FriendsListFragment())
+            .addToBackStack(null)
+            .commit()
 
-        fragmentManager.beginTransaction().replace(binding.fragmentFriendList.id,fragment).commit()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_search,menu)
-
-        val menuItem = menu?.findItem(R.id.action_search)
-        val searchView = menuItem?.actionView as androidx.appcompat.widget.SearchView
-        searchView.setOnQueryTextListener(object :androidx.appcompat.widget.SearchView.OnQueryTextListener{
+    private fun setOnQueryTextListener() {
+        binding.searchFriend.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                return false
+
+                return true
             }
         })
-        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun addFriends(friendId:String){
+        val user = Firebase.auth.currentUser
+        val uid = FBAuth.getUid()
+
+        if(user != null){
+            // User is signed in
+
+            FBRef.friendsRef.child(uid).child("friends").push().setValue(friendId)
+                .addOnSuccessListener {
+                    Log.d(uid,"친구 추가 성공")
+                    FBRef.friendsRef.child(friendId).child("friends").push().setValue(uid)
+                }
+                .addOnFailureListener{ Log.d(uid,"친구 추가 실패")}
+
+        } else{
+            Toast.makeText(this,"로그인을 해주세요",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun getFriendsList(callback: (List<String>) -> Unit){
+        val uid = FBAuth.getUid()
+        FBRef.friendsRef.child(uid).child("friends").get()
+            .addOnSuccessListener { snapshot ->
+            if(snapshot.exists()){
+                val friends = snapshot.children.mapNotNull { it.key }
+                callback(friends)
+            }
+        }
+            .addOnFailureListener {
+                Log.d("listFriends","fail")
+                callback(emptyList())
+            }
     }
 }
-
-//manifest에 임시로 parent poceketBoard로 해놓음
--- src/main/java/com/practice/android/pocketmate/friends/FriendsListFragment.kt
+++ src/main/java/com/practice/android/pocketmate/friends/FriendsListFragment.kt
@@ -1,17 +1,26 @@
+package com.practice.android.pocketmate.friends
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.practice.android.pocketmate.R
+import com.practice.android.pocketmate.databinding.FragmentFriendsListBinding
 
 class FriendsListFragment : Fragment() {
+    lateinit var binding : FragmentFriendsListBinding
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_friends_list, container, false)
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFriendsListBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
 }
