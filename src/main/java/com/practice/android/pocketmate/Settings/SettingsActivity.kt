086012c9443a0f6f165d5cf9aaae2bc8b400175f
Han HaeWoon <dorothy8380@gmail.com>
2024-12-04 00:53:11 +0900

    툴바, 업버튼 설정

--- src/main/java/com/practice/android/pocketmate/Settings/SettingsActivity.kt
+++ src/main/java/com/practice/android/pocketmate/Settings/SettingsActivity.kt
@@ -2,6 +2,8 @@ package com.practice.android.pocketmate.Settings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
@@ -11,9 +13,23 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbarSettings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         supportFragmentManager.beginTransaction()
-            .replace(android.R.id.content, SettingsFragment())
+            .replace(R.id.fragment_settings, SettingsFragment())
             .commit()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if(supportFragmentManager.backStackEntryCount > 0){
+            supportFragmentManager.popBackStack()
+
+            val toolbar = findViewById<Toolbar>(R.id.toolbar_settings)
+            toolbar?.title = "설정"
+            return true
+        }
+        return super.onSupportNavigateUp()
     }
 }
