ae5266ff1285f0bde287f96ca8fd5bd88645a225
Han HaeWoon <dorothy8380@gmail.com>
2024-12-05 01:04:44 +0900

    통계 기능 구현

--- src/main/java/com/practice/android/pocketmate/Adapter/PocketBoardAdapter.kt
+++ src/main/java/com/practice/android/pocketmate/Adapter/PocketBoardAdapter.kt
@@ -1,10 +1,15 @@
 package com.practice.android.pocketmate.Adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.view.LayoutInflater
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.practice.android.pocketmate.Model.BoardModel
 import com.practice.android.pocketmate.Pocket.PocketActivity
+import com.practice.android.pocketmate.R
 import com.practice.android.pocketmate.databinding.ItemBoardBinding
 
 class PocketBoardAdapter(context: Context,
@@ -19,6 +24,28 @@ class PocketBoardAdapter(context: Context,
         bindItems(binding, tip, key)
         binding.root.setOnClickListener {
             switchScreenWithKey(key)
+        }
+
+        binding.statisticsBtn.visibility = View.VISIBLE
+        binding.statisticsBtn.setOnClickListener{
+            val agree = binding.agreeNumber.text.toString().toDouble()
+            val disagree = binding.disagreeNumber.text.toString().toDouble()
+            val agreeRatio : Double
+            val disagreeRatio : Double
+
+            if(disagree != 0.0) {
+                agreeRatio = agree / (agree + disagree)
+            }
+            else
+                agreeRatio = 100.0
+
+            if(agree != 0.0) {
+                disagreeRatio = disagree / (agree + disagree)
+            }
+            else
+                disagreeRatio = 100.0
+
+            showDialog(context,agreeRatio,disagreeRatio)
         }
     }
 
@@ -43,5 +70,55 @@ class PocketBoardAdapter(context: Context,
         showCommentCount(key, binding)
         showPostReaction(key, binding)
 
+    }
+
+    private fun showDialog(context: Context,agree:Double,disagree: Double){
+        val dialogLayout = LayoutInflater.from(context).inflate(R.layout.dialog_pie,null)
+        val dialog = AlertDialog.Builder(context).setView(dialogLayout).create()
+
+        dialog.show()
+
+        val webView : WebView = dialogLayout.findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        val htmlCode = loadPie(agree, disagree)
+        webView.loadDataWithBaseURL(null, htmlCode,"text/html","UTF-8",null)
+    }
+
+    private fun loadPie(agree: Double,disagree:Double): String {
+        val htmlCode = """
+            <!DOCTYPE html>
+            <html>
+              <head>
+                <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                <script type="text/javascript">
+                  google.charts.load('current', {packages:['corechart']});
+                  google.charts.setOnLoadCallback(drawChart);
+
+                  function drawChart() {
+                    var data = google.visualization.arrayToDataTable([
+                      ['Category', 'Value'],
+                      ['찬성', $agree],
+                      ['반대', $disagree]                     
+                    ]);
+
+                    var options = {
+                      title: '유저 의견',
+                      is3D: false,
+                    };
+
+                    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                    chart.draw(data, options);
+                  }
+                </script>
+              </head>
+              <body>
+                <div id="piechart" style="width: 100%; height: 100%;"></div>
+              </body>
+            </html>
+
+        """.trimIndent()
+        return htmlCode
     }
 }
